9318d8f253c350fb1c2cf74ca1305d325b343187
pradeep thirvaskur <142670238+PradeepThirvaskur@users.noreply.github.com>
2024-10-18 12:55:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 //your JS code here. If required.
  let origin = window.location.href;
-    return alert("the length of the URl is :" +origin.length)
+    alert("the length of the URl is :" +origin.length)
